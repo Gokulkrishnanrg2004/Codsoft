6f17c09e3449b3eb8b91ac0d37f3e041ee30453f
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2023-11-13 19:13:19 +0530

    Update tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
+char grid[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
-int checkwin();
+int ifwin();
 void board();
 
 int main()
@@ -20,33 +20,33 @@ int main()
 
         mark=(player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && square[1] == '1')
+        if (choice == 1 && grid[1] == '1')
 
-            square[1] = mark;
-        else if (choice == 2 && square[2] == '2')
+            grid[1] = mark;
+        else if (choice == 2 && grid[2] == '2')
 
-            square[2] = mark;
-        else if (choice == 3 && square[3] == '3')
+            grid[2] = mark;
+        else if (choice == 3 && grid[3] == '3')
 
-            square[3] = mark;
-        else if (choice == 4 && square[4] == '4')
+            grid[3] = mark;
+        else if (choice == 4 && grid[4] == '4')
 
-            square[4] = mark;
-        else if (choice == 5 && square[5] == '5')
+            grid[4] = mark;
+        else if (choice == 5 && grid[5] == '5')
 
-            square[5] = mark;
-        else if (choice == 6 && square[6] == '6')
+            grid[5] = mark;
+        else if (choice == 6 && grid[6] == '6')
 
-            square[6] = mark;
-        else if (choice == 7 && square[7] == '7')
+            grid[6] = mark;
+        else if (choice == 7 && grid[7] == '7')
 
-            square[7] = mark;
-        else if (choice == 8 && square[8] == '8')
+            grid[7] = mark;
+        else if (choice == 8 && grid[8] == '8')
 
-            square[8] = mark;
-        else if (choice == 9 && square[9] == '9')
+            grid[8] = mark;
+        else if (choice == 9 && grid[9] == '9')
 
-            square[9] = mark;
+            grid[9] = mark;
         else
         {
             cout<<"Invalid move ";
@@ -55,7 +55,7 @@ int main()
             cin.ignore();
             cin.get();
         }
-        i=checkwin();
+        i=ifwin();
 
         player++;
     }while(i==-1);
@@ -70,35 +70,35 @@ int main()
     cin.get();
     return 0;
 }
-int checkwin()
+int ifwin()
 {
-    if (square[1] == square[2] && square[2] == square[3])
+    if (grid[1] == grid[2] && grid[2] == grid[3])
 
         return 1;
-    else if (square[4] == square[5] && square[5] == square[6])
+    else if (grid[4] == grid[5] && grid[5] == grid[6])
 
         return 1;
-    else if (square[7] == square[8] && square[8] == square[9])
+    else if (grid[7] == grid[8] && grid[8] == grid[9])
 
         return 1;
-    else if (square[1] == square[4] && square[4] == square[7])
+    else if (grid[1] == grid[4] && grid[4] == grid[7])
 
         return 1;
-    else if (square[2] == square[5] && square[5] == square[8])
+    else if (grid[2] == grid[5] && grid[5] == grid[8])
 
         return 1;
-    else if (square[3] == square[6] && square[6] == square[9])
+    else if (grid[3] == grid[6] && grid[6] == grid[9])
 
         return 1;
-    else if (square[1] == square[5] && square[5] == square[9])
+    else if (grid[1] == grid[5] && grid[5] == grid[9])
 
         return 1;
-    else if (square[3] == square[5] && square[5] == square[7])
+    else if (grid[3] == grid[5] && grid[5] == grid[7])
 
         return 1;
-    else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
-                    && square[4] != '4' && square[5] != '5' && square[6] != '6'
-                  && square[7] != '7' && square[8] != '8' && square[9] != '9')
+    else if (grid[1] != '1' && grid[2] != '2' && grid[3] != '3'
+                    && grid[4] != '4' && grid[5] != '5' && grid[6] != '6'
+                  && grid[7] != '7' && grid[8] != '8' && grid[9] != '9')
 
         return 0;
     else
@@ -112,17 +112,17 @@ void board()
     cout << endl;
 
     cout << "     |     |     " << endl;
-    cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+    cout << "  " << grid[1] << "  |  " << grid[2] << "  |  " << grid[3] << endl;
 
     cout << "_____|_____|_____" << endl;
     cout << "     |     |     " << endl;
 
-    cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+    cout << "  " << grid[4] << "  |  " << grid[5] << "  |  " << grid[6] << endl;
 
     cout << "_____|_____|_____" << endl;
     cout << "     |     |     " << endl;
 
-    cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+    cout << "  " << grid[7] << "  |  " << grid[8] << "  |  " << grid[9] << endl;
 
     cout << "     |     |     " << endl << endl;
 }
